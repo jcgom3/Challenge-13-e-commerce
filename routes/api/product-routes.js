2a88bd9608e14e5b9e59eedf4816fec6ac9059d7
Juan  Gomez <jcgom3@icloud.com>
2020-11-01 22:12:18 -0500

    created routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,61 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: [ 'id',
+                  'product_name',
+                  'price',
+                  'stock',
+                  'category_id',
+                  [sequelize.literal('(SELECT COUNT(*) from product_tag WHERE product.id = product_tag.product_id)'),'tags']
+                ],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      }
+    ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    res.status(500).json(err)
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [ 'id',
+                  'product_name',
+                  'price',
+                  'stock',
+                  'category_id'
+                ],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+        through: ProductTag,
+        as: 'tags'
+      }
+    ]
+  })
+  .then(dbProductData => {
+    if(!dbProductData) {
+      res.status(404).json({ message: 'No product found with current id' });
+      return;
+    }
+    res.json(dbProductData)
+  });
 });
 
 // create new product
